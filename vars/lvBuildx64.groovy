87d5adbd07466cd0e68f2521f41b90809ed2242b
Andy Church <andy.church@ni.com>
2017-09-15 09:05:12 -0700

    Fixed syntax in lvBuildx64 step.

--- vars/lvBuildx64.groovy
+++ vars/lvBuildx64.groovy
@@ -3,5 +3,5 @@ def call(path, target_name, spec_name, lv_version, temp_build_path) {
 		//This step calls the 64-bit LabVIEW application to build the specified build spec within the project.
 		
         echo "Build the build spec: ${spec_name} under target ${target_name} in project at ${path}"
-        bat "labview-cli --kill --lv-ver --x64 ${lv_version} \"L:\\lv-build-system\\steps\\lv-build.vi\" -- \"${path}\" \"${target_name}\" \"${spec_name}\" \"${temp_build_path}\" \"${WORKSPACE}\""
+        bat "labview-cli --kill --x64 --lv-ver ${lv_version} \"L:\\lv-build-system\\steps\\lv-build.vi\" -- \"${path}\" \"${target_name}\" \"${spec_name}\" \"${temp_build_path}\" \"${WORKSPACE}\""
 }
