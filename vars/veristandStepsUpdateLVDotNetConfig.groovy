8eb224c6e013640b4312b49fb9d20bcc79e1f4db
Andy Church <andy.church@ni.com>
2017-09-14 19:11:33 -0700

    Fixed bug in step parameters.

--- vars/veristandStepsUpdateLVDotNetConfig.groovy
+++ vars/veristandStepsUpdateLVDotNetConfig.groovy
@@ -1,8 +1,8 @@
-def call(dotnet_config_filepath, veristand_version, teststand_version) {
+def call(lv_version, dotnet_config_filepath, veristand_version, teststand_version) {
 
 		//This build step updates the .NET assembly binding redirection configuration files that allow LabVIEW to load the specified version of the VeriStand and TestStand assemblies. 
 		//Use the LabVIEW version that matches VeriStand because you know that version will be available on the node where this executes. 
 		
         echo "Updating \"${dotnet_config_filepath}\" assembly versions to Veristand ${veristand_version} and TestStand ${teststand_version}."
-        bat "labview-cli --kill --lv-ver ${veristand_version} \"L:\\veristand-steps-build-system\\steps\\veristandSteps-lvDotNetConfigFile-update.vi\" -- \"${dotnet_config_filepath}\" \"${dotnet_config_filepath}\" \"${teststand_version}\" \"${veristand_version}\""
+        bat "labview-cli --kill --lv-ver ${lv_version} \"L:\\veristand-steps-build-system\\steps\\veristandSteps-lvDotNetConfigFile-update.vi\" -- \"${dotnet_config_filepath}\" \"${dotnet_config_filepath}\" \"${teststand_version}\" \"${veristand_version}\""
 }
