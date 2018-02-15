e31a3508d4d878de7078afb9caeb266d4f8a1ce9
Andy Church <church.andrew.d@gmail.com>
2018-02-15 09:21:19 -0600

    added IntegrationPipelineExecutor

---
+++ src/ni/vsbuild/IntegrationPipelineExecutor.groovy
@@ -0,0 +1,12 @@
+package ni.vsbuild
+
+class PipelineExecutor implements Serializable {
+
+   static void execute(script, List<String> lvVersions, List<String> dependencies = []) {
+      def pipelineInformation = new PipelineInformation('veristand', lvVersions, dependencies)
+      pipelineInformation.printInformation(script)
+
+      def pipeline = new IntegrationPipeline(script, pipelineInformation)
+      pipeline.execute()
+   }
+}
