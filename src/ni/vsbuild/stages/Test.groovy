ad02d53354426537cbf3ac05d4f26bb6e214debb
Andy Church <church.andrew.d@gmail.com>
2018-02-15 09:39:40 -0600

    added stage name 'Test' to constructor

--- src/ni/vsbuild/stages/Test.groovy
+++ src/ni/vsbuild/stages/Test.groovy
@@ -3,7 +3,7 @@ package ni.vsbuild.stages
 class Test extends AbstractStepStage {
 
    Test(script, configuration, lvVersion) {
-      super(script, configuration, lvVersion)
+      super(script, 'Test', configuration, lvVersion)
    }
 
    void executeStage() {
