7c1644fac86fe5a42cfce0c26c616233276aae8a
Andy Church <church.andrew.d@gmail.com>
2018-02-15 10:13:09 -0600

    changed AbstractStep constructor in IntegrationTest.groovy

--- src/ni/vsbuild/steps/IntegrationTest.groovy
+++ src/ni/vsbuild/steps/IntegrationTest.groovy
@@ -6,11 +6,13 @@ class IntegrationTest extends AbstractStep {
 
    def seqPath
    def tsVersion
+   def lvVersion
 
    IntegrationTest(script, mapStep, lvVersion) {
-      super(script, mapStep, lvVersion)
+      super(script, mapStep)
       this.seqPath = mapStep.get('sequence_path')
       this.tsVersion = mapStep.get('teststand_version')
+      this.lvVersion = lvVersion
    }
 
    void executeStep(BuildConfiguration buildConfiguration) {
