fbeecd6e1bb9c1a635f21f726fbcb77611ae4bb4
Andy Church <church.andrew.d@gmail.com>
2018-02-15 10:18:56 -0600

    added escapes to batch call in runIntegrationTest

--- vars/runIntegrationTest.groovy
+++ vars/runIntegrationTest.groovy
@@ -2,5 +2,5 @@ def call(seqPath, tsVersion) {
    echo "Running test $seqPath with TestStand $tsVersion"
    def seqEditorPath = "C:\\Program Files (x86)\\National Instruments\\TestStand ${tsVersion}\\Bin\\SeqEdit.exe"
 
-   bat "${seqEditorPath} /run MainSequence ${seqPath}"
+   bat "\'${seqEditorPath}\' /run MainSequence \'${seqPath}\'"
 }
