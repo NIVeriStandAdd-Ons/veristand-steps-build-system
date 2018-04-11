2ef90d71ede728393765d33cab8b0a62273f2a14
Andy Church <church.andrew.d@gmail.com>
2018-04-11 15:13:59 -0500

    create nipkg source directories

--- src/ni/vsbuild/packages/VsStepsPackage.groovy
+++ src/ni/vsbuild/packages/VsStepsPackage.groovy
@@ -2,24 +2,26 @@ package ni.vsbuild.packages
 
 class VsStepsPackage extends AbstractPackage {
 
-   def version
+   def nipkgVersion
    def tsVersions
+   def vsVersion
 
    VsStepsPackage(script, packageInfo, payloadDir) {
       super(script, packageInfo, payloadDir)
-      this.version = packageInfo.get('version')
+      this.nipkgVersion = packageInfo.get('version')
       this.tsVersions = packageInfo.get('teststand_versions')
+      this.vsVersion = lvVersion
    }
 
    void buildPackage(lvVersion) {
       def packageInfo = """
          Building package $name from $payloadDir
-         Package version: $version
+         Package version: $nipkgVersion
          TestStand versions: $tsVersions
          """.stripIndent()
 
       script.echo packageInfo
-      script.vsStepsPackage(version, tsVersions, payloadDir, lvVersion)
+      script.vsStepsPackage(nipkgVersion, tsVersions, payloadDir, vsVersion)
 
    }
 }
--- vars/vsStepsPackage.groovy
+++ vars/vsStepsPackage.groovy
@@ -1,18 +1,16 @@
-def call(version, tsVersions, payloadDir, lvVersion) {
+def call(version, tsVersions, payloadDir, vsVersion) {
 
-   def vsVersion = lvVersion
-   
    if(vsVersion == 2015) {
       vsVersion = "2015sp1"
    }
 
    tsVersions.each{tsVersion ->
-      def packageSource = "Source\\LV\\Installer\\built\\teststand${tsVersion}\\volume" //Example: 'Source\\LV\\Installer\\Built\\teststand2014\\volume'
-      def zipFileName = "veristand${vsVersion}-steps-teststand${tsVersion}-v${version}.zip" //Example: 'veristand2015sp1-steps-for-teststand2014-v7.1.0'
       
-      echo "Building ${zipFileName} from ${packageSource}."
-
-      zip zipFile: "Built\\${zipFileName}", dir: "${packageSource}"
+      def nipkgDir = "nipkg\\veristand${vsVersion}-steps-teststand${tsVersion}"
+      
+      bat 'dir \"${nipkgDir}\"'
+      
+      echo "Creating folder ${nipkgDir}."
 
    }
 }
