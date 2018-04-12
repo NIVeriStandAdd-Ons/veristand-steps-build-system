48594fa3a8771536451bc523def99a78579d9f4d
Andy Church <church.andrew.d@gmail.com>
2018-04-11 21:08:43 -0500

    create staging directories, debian-binary file

--- vars/vsStepsPackage.groovy
+++ vars/vsStepsPackage.groovy
@@ -6,10 +6,15 @@ def call(version, tsVersions, payloadDir, vsVersion) {
 
    def controlFileText = readFile "control"
    echo controlFileText
+   
+   def programFilesStagingDirectory = "data\\ProgramFiles_32\\VeriStand Steps for TestStand"
+   echo programFilesStagingDirectory
 
    tsVersions.each{tsVersion ->
       
       def nipkgDir = "nipkg\\veristand${vsVersion}-steps-teststand${tsVersion}"
+      def tsPublicDocs = "documents\\National Instruments\\TestStand ${tsVersion} (32-bit)"
+      echo tsPublicDocs
       def updatedControlText = controlFileText
 
       updatedControlText.replaceAll("\\{veristand_version\\}", "${vsVersion}")
@@ -18,7 +23,8 @@ def call(version, tsVersions, payloadDir, vsVersion) {
       echo updatedControlText
        
       dir(nipkgDir){
-         writeFile file:'control\control', text: updatedControlText
+         writeFile file:'control\\control', text: updatedControlText
+         writeFile file:'debian-binary', text: "2.0"
       }
       
       echo "Creating folder ${nipkgDir}."
