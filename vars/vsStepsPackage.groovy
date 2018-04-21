6585745e134ea16f01b1d8af0a6568bd023cd1af
Andy Church <church.andrew.d@gmail.com>
2018-04-21 14:01:28 -0500

    copy built files to nipkg staging directories

--- vars/vsStepsPackage.groovy
+++ vars/vsStepsPackage.groovy
@@ -1,7 +1,3 @@
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-
 def call(nipkgVersion, tsVersions, payloadDir, vsVersion) {
 
    if(vsVersion == "2015") {
@@ -18,7 +14,14 @@ def call(nipkgVersion, tsVersions, payloadDir, vsVersion) {
    tsVersions.each{tsVersion ->
       def replacementExpressionMap = ['veristand_version': vsVersion,  'teststand_version': tsVersion, 'nipkg_version': nipkgVersion] 
       def nipkgDir = "nipkg\\veristand${vsVersion}-steps-teststand${tsVersion}"
+      def programFilesStagingSource = "built\\programFiles_32"
+      def programFilesStagingDest = "nipkg\\data\\programFiles_32"
+      def documentsStagingSource = "built\\documents"
+      def documentsStagingDest = "nipkg\\documents"
       def updatedControlText = controlFileText
+
+      bat "(robocopy \"${programFilesStagingSource}\" \"{programFilesStagingDest}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
+      bat "(robocopy \"${documentsStagingSource}\" \"{documentsStagingDest}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
 
       replacementExpressionMap.each { replacementExpression, replacementValue ->
          updatedControlText = updatedControlText.replaceAll("\\{${replacementExpression}\\}", replacementValue)
