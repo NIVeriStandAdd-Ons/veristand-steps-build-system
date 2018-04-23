ad4abbbe004e797499f04742c29a6b615e90a0b2
Andy Church <church.andrew.d@gmail.com>
2018-04-23 14:19:27 -0500

    add lvVersion parameter

--- src/ni/vsbuild/Pipeline.groovy
+++ src/ni/vsbuild/Pipeline.groovy
@@ -115,7 +115,7 @@ class Pipeline implements Serializable {
       }
       script.stage("Setup_$lvVersion") {
          script.cloneCommonbuild()
-         script.buildSetup()
+         script.buildSetup(lvVersion)
       }
    }
 }
