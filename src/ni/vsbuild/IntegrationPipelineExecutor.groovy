c3c581671cc12bd9a13d864f98da74e8d3e4b4a1
Andy Church <church.andrew.d@gmail.com>
2018-04-25 09:36:22 -0500

    fix typo (testing integration test)

--- src/ni/vsbuild/IntegrationPipelineExecutor.groovy
+++ src/ni/vsbuild/IntegrationPipelineExecutor.groovy
@@ -1,6 +1,6 @@
 package ni.vsbuild
 
-class PipelineExecutor implements Serializable {
+class IntegrationPipelineExecutor implements Serializable {
 
    static void execute(script, List<String> lvVersions, List<String> dependencies = []) {
       def pipelineInformation = new PipelineInformation('veristand', lvVersions, dependencies)
